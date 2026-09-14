38adc8ab99179771cb5830d74cd247b6c5440303
ukp25cs060-arch <ukp25cs060@ukfcet.ac.in>
2026-09-14 09:40:26 +0530

    Create Output.c

---
+++ Output.c
@@ -0,0 +1,69 @@
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 1
+Enter patient name: P1
+Enter priority (1=Emergency, 2=Urgent, 3=Routine): 3
+Patient added successfully.
+
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 1
+Enter patient name: P2
+Enter priority (1=Emergency, 2=Urgent, 3=Routine): 1
+Patient added successfully.
+
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 1
+Enter patient name: P2
+Enter priority (1=Emergency, 2=Urgent, 3=Routine): 2
+Patient added successfully.
+
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 1
+Enter patient name: P4
+Enter priority (1=Emergency, 2=Urgent, 3=Routine): 1
+Patient added successfully.
+
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 1
+Enter patient name: P5
+Enter priority (1=Emergency, 2=Urgent, 3=Routine): 3
+Patient added successfully.
+
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 1
+Enter patient name: P6
+Enter priority (1=Emergency, 2=Urgent, 3=Routine): 2
+Patient added successfully.
+
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 2
+Patient treated: P2
+Priority: 1
+
+===== Hospital Patient Priority Queue =====
+1. Add Patient
+2. Treat Next Patient
+3. Exit
+Enter your choice: 2
+Patient treated: P4
+Priority: 1
